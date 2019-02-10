491e99875605e2e508562d9169287a4a33023e98
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-02-10 18:46:05 +0530

    Create begnset8 3.c

---
+++ begnset8 3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+int i,f=0;
+clrscr();
+scanf("%s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+f=1;
+}
+if(f==1)
+printf("yes");
+else
+printf("no");
+getch();
+}
